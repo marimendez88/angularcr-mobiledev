5b9c572e0de2b3be9bc6f0cfcaf77f82fee81fc5
Maria Jose Mendez <majomendez88mena@gmail.com>
2021-03-06 16:07:44 -0600

    feat(mobilecss): adding mobile frame in css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private platform: Platform) {}
+
+  ngOnInit() {
+    if (!this.platform.is('hybrid') && !this.platform.is('mobileweb')) {
+      const element = document.getElementsByTagName('ion-app')[0];
+      element.classList.add('desktop');
+    }
+  }
 }
