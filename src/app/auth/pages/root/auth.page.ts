65b12145725ea71520e4d2ede9dd8f0b886b8845
Maria Jose Mendez <majomendez88mena@gmail.com>
2021-03-06 17:17:58 -0600

    feat(auth pag): some progress

--- src/app/auth/pages/root/auth.page.ts
+++ src/app/auth/pages/root/auth.page.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
   templateUrl: 'auth.page.html',
   styleUrls: ['auth.page.scss'],
 })
-export class AuthPage {
+export class AuthPage implements OnInit {
   constructor() {}
+
+  loginSection: boolean;
+  signUpSection: boolean;
+  rootSection: boolean = true;
+
+  ngOnInit() {}
+
+  changeSection(key: string) {
+    switch (key) {
+      case 'loginSection':
+        this.loginSection = true;
+        this.signUpSection = false;
+        this.rootSection = false;
+        break;
+      case 'signUpSection':
+        this.loginSection = false;
+        this.signUpSection = true;
+        this.rootSection = false;
+        break;
+      case 'rootSection':
+        this.loginSection = false;
+        this.signUpSection = false;
+        this.rootSection = true;
+        break;
+
+      default:
+        break;
+    }
+  }
 }
